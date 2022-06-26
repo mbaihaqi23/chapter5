a4c0db2427b66ce5e1918def7eb786e0ddc8a385
mbaihaqi23 <mbaihaqi23@gmail.com>
2022-06-26 23:50:03 +0700

    PR crud blom selesai

---
+++ PR_crud/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const usersList = [{
+    name : "rico",
+    password : "123as",
+    id : "11"
+}];
+
+app.get("/", (req, res) => {
+  res.send("hello from simple server :)");
+});
+
+// api untuk mendapat semua users
+app.get("/users", (req, res) => {
+   return res.json(usersList);
+ });
+
+// api untuk membuat sebuah users
+// app.post("/users", (req, res) => {
+// harus bisa menerima input dari frontend
+//     const newUser = req.body;
+//     const newUserId = pikirkan cara ganeratenya
+
+//     usersList.push({...newUser, id: newUserId});
+//     return res.json({...newUser, id: newUserId});
+// });
+
+// mendapatkan 1 user saja
+// app.get("/users/:userId", (req, res) => {
+//     const {userId} = req.params;
+
+// filter userList dan return user dengan id yang sama
+//     return res.json(user dengan id yang sama); 
+// });
+
+app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
